f58922c930347c4e840c97a42d31e09850025106
laviate <36458681+laviate@users.noreply.github.com>
2018-03-20 01:39:33 +0200

    Added DC motor speed control

---
+++ esp.ino
@@ -0,0 +1,115 @@
+#include<SPI.h>
+#include <ESP8266WiFi.h>
+
+char ssid[] = "esp_ap";       //   SSID 
+char pass[] = "12345678";    // паролааа
+
+WiFiServer server(80);
+
+char buff[]="F\n";
+char buff1[]="S\n";
+char buff2[]="R\n";
+char buff3[]="B\n";
+char buff4[]="L\n";
+
+String request = "";
+
+void setup() {
+ Serial.begin(115200); 
+ SPI.begin();  
+
+ WiFi.mode(WIFI_AP);         
+ WiFi.softAP(ssid, pass);  
+ IPAddress myIP = WiFi.softAPIP(); 
+ Serial.print("HotSpt IP:");
+ Serial.println(myIP);
+ 
+  
+ server.begin();              
+ Serial.println("HTTP server started");
+}
+
+void loop() {
+
+
+  WiFiClient client = server.available();
+    if (!client)  {  return;  }
+ 
+    request = client.readStringUntil('\n');
+
+    if( request.indexOf("FORWARD") > 0 )  
+    { 
+      
+      for(int i=0; i<sizeof buff; i++)  
+      {
+        SPI.transfer(buff[i]);
+      }
+      delay(1000); 
+    }
+    else if( request.indexOf("STOP") > 0 ) 
+    { 
+      for(int i=0; i<sizeof buff1; i++) 
+      {
+        SPI.transfer(buff1[i]);
+      }
+      delay(1000);  
+    }
+    else if( request.indexOf("RIGHT") > 0 ) 
+    { 
+      for(int i=0; i<sizeof buff2; i++) 
+      {
+        SPI.transfer(buff2[i]);
+      }
+      delay(1000);   
+    }
+    else if( request.indexOf("BACKWARD") > 0 ) 
+    { 
+      for(int i=0; i<sizeof buff3; i++) 
+        SPI.transfer(buff3[i]);
+      delay(1000);   
+    }
+
+    else if( request.indexOf("LEFT") > 0 ) 
+    { 
+      for(int i=0; i<sizeof buff4; i++)  
+        SPI.transfer(buff4[i]);
+      delay(1000);   
+    }
+
+  client.flush();
+
+  client.print(F(  
+    "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"
+    "<!DOCTYPE html>"
+    "<html>"
+    "<head>"
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'/>"
+    "<meta charset='utf-8'>"
+    "<style>"
+    "body {font-size:140%;}"
+    "#main {display: table; margin: auto;  padding: 0 10px 0 10px; }"
+    "h2 {text-align:center; } "
+    ".button { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;} "
+    ".button1 { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}"
+    ".button2 { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}"
+    ".button3 { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}"
+    ".button4 { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}"
+    "</style>"
+    "<title>Walkin Robot Control</title>"
+    "</head>"
+    "<body>"
+    "<div id='main'><h2>WALKING ROBOT</h2>"
+    "<form id='F1' action='FORWARD'><input class='button' type='submit' value='MOVE FORWARD' ></form><br>"
+    "<form id='F4' action='BACKWARD'><input class='button3' type='submit' value='BACKWARD' ></form><br>"
+    "<form id='F3' action='RIGHT'><input class='button2' type='submit' value='RIGHT' ></form><br>"
+    "<form id='F5' action='LEFT'><input class='button4' type='submit' value='LEFT' ></form><br>"
+    "<form id='F2' action='STOP'><input class='button1' type='submit' value='STOP' ></form><br>"
+    "</div>"
+    "</body>"
+    "</html>"
+    ));   
+
+ 
+    delay(5);
+}
+
--- spi_arduino.ino
+++ spi_arduino.ino
@@ -1,11 +1,12 @@
-
 #include <SPI.h>
 
-const int M11 = 3;
+const int M11 = 2;
 const int M12 = 4;
 const int M22 = 5;
-const int M21 = 6;
-const int sl = 7;
+const int M21 = 1;
+const int speed1 = 3;
+const  int speed2 = 6;
+const int sl = 0;
 const int s = 8;
 const int sr = 9;
 char DM;
@@ -21,26 +22,35 @@ void setup() {
    pinMode(M12, OUTPUT);
    pinMode(M22, OUTPUT);
    pinMode(M21, OUTPUT);
+   pinMode(speed1, OUTPUT);
+   pinMode(speed2, OUTPUT);
+   
    pinMode(sl, INPUT);
    pinMode(s, INPUT);
    pinMode(sr, INPUT);
-   Serial.begin (9600);
+   
+
 
    SPCR |= bit(SPE);         /* Enable SPI */
    pinMode(MISO, OUTPUT);    /* Make MISO pin as OUTPUT */
    index = 0;
    receivedone = false;
    SPI.attachInterrupt();    /* Attach SPI interrupt */
+   Serial.begin(9600);      // open the serial port at 9600 bps:
+   
+
+
 }
 
 void loop() {
   if (receivedone)          /* Check and print received buffer if any */
   {
     buff[index] = 0;
-    Serial.println(buff);
+    //Serial.println(buff);
     index = 0;
     receivedone = false;
   }
+  
 }
 
 
@@ -56,31 +66,68 @@ ISR (SPI_STC_vect)
     //if (c == '\n'){     /* Check for newline character as end of msg */
     // receivedone = true;
     //}
-
-    if(c=='F')
+    if(c=='Fs')
     {
-      digitalWrite(M11, LOW);
-      digitalWrite(M12, LOW);
-      digitalWrite(M22, HIGH);
-      digitalWrite(M21, LOW);
-      receivedone = true; 
-    }
-    if(c=='B')
-    { 
+      analogWrite(speed2, 40); // Set rotating motor speed
       digitalWrite(M11, LOW);
       digitalWrite(M12, LOW);
       digitalWrite(M22, LOW);
       digitalWrite(M21, HIGH);
+      receivedone = true; 
+    }
+    if(c=='Ff')
+    {
+      analogWrite(speed2, 230); // Set rotating motor speed
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, LOW);
+      digitalWrite(M22, LOW);
+      digitalWrite(M21, HIGH);
+      receivedone = true; 
+    }
+    if(c=='Bs')
+    {
+      analogWrite(speed2, 40); // Set rotating motor speed 
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, LOW);
+      digitalWrite(M22, HIGH);
+      digitalWrite(M21, LOW);
       receivedone = true;
     }
-    if(c=='L'){
+    if(c=='Bf')
+    { 
+      analogWrite(speed2, 230); // Set rotating motor speed 
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, LOW);
+      digitalWrite(M22, HIGH);
+      digitalWrite(M21, LOW);
+      receivedone = true;
+    }
+    if(c=='Ls'){
+      analogWrite(speed1, 40); // Set rotating motor speed
       digitalWrite(M11, LOW);
       digitalWrite(M12, HIGH);
       digitalWrite(M22, LOW);
       digitalWrite(M21, LOW);
       receivedone = true;
     }
-    if(c=='R'){
+    if(c=='Lf'){
+      analogWrite(speed1, 230); // Set rotating motor speed
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, HIGH);
+      digitalWrite(M22, LOW);
+      digitalWrite(M21, LOW);
+      receivedone = true;
+    }
+    if(c=='Rs'){
+      analogWrite(speed1, 40); // Set rotating motor speed
+      digitalWrite(M11, HIGH);
+      digitalWrite(M12, LOW);
+      digitalWrite(M22, LOW);
+      digitalWrite(M21, LOW);
+      receivedone = true;
+    }
+    if(c=='Rf'){
+      analogWrite(speed1, 230); // Set rotating motor speed
       digitalWrite(M11, HIGH);
       digitalWrite(M12, LOW);
       digitalWrite(M22, LOW);
@@ -88,6 +135,8 @@ ISR (SPI_STC_vect)
       receivedone = true;
     }
     if(c=='S'){
+      analogWrite(speed1, 0); // Set rotating motor speed
+      analogWrite(speed2, 0); // Set rotating motor speed
       digitalWrite(M11, LOW);
       digitalWrite(M12, LOW);
       digitalWrite(M22, LOW);
