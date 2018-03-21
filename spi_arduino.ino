20e0ba90c5f5e49dd7674a79752802d6611273e3
bibishte <bibishte@gmail.com>
2018-03-21 21:26:11 +0200

    updated with sensors not turning

--- spi_arduino.ino
+++ spi_arduino.ino
@@ -5,9 +5,9 @@ const int M12 = 4;
 const int M22 = 6;
 const int M21 = 7;
 const int speed1 = 3;
-const int sl = 0;
-const int s = 8;
-const int sr = 9;
+const int sl = A0;
+const int s = A1;
+const int sr = A2;
 char DM;
 int r, f, l;
 
@@ -24,9 +24,9 @@ void setup() {
    pinMode(speed1, OUTPUT);
 
    
-   /*pinMode(sl, INPUT);
+   pinMode(sl, INPUT);
    pinMode(s, INPUT);
-   pinMode(sr, INPUT);*/
+   pinMode(sr, INPUT);
    
 
 
@@ -48,7 +48,26 @@ void loop() {
     //Serial.println(buff);
     index = 0;
     receivedone = false;
+    
   }
+   if((digitalRead(s)==LOW) || (digitalRead(sl)==LOW) || (digitalRead(sr)==LOW))
+   //if(digitalRead(sl)==LOW)
+    {
+      analogWrite(speed1, 60); // Set rotating motor speed
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, HIGH);
+      digitalWrite(M22, LOW);
+      digitalWrite(M21, LOW);
+      //delay(1000);
+      //break;
+    }else
+    {
+      digitalWrite(M11, LOW);
+      digitalWrite(M12, LOW);
+      digitalWrite(M22, LOW);
+      digitalWrite(M21, LOW);
+    }
+  
   
 }
 
@@ -73,24 +92,8 @@ ISR (SPI_STC_vect)
       digitalWrite(M21, HIGH);
       receivedone = true; 
     }
-    if(c=='Q')
-    {
-      digitalWrite(M11, LOW);
-      digitalWrite(M12, LOW);
-      digitalWrite(M22, LOW);
-      digitalWrite(M21, HIGH);
-      receivedone = true; 
-    }
     if(c=='B')
     {
-      digitalWrite(M11, LOW);
-      digitalWrite(M12, LOW);
-      digitalWrite(M22, HIGH);
-      digitalWrite(M21, LOW);
-      receivedone = true;
-    }
-    if(c=='W')
-    { 
       digitalWrite(M11, LOW);
       digitalWrite(M12, LOW);
       digitalWrite(M22, HIGH);
@@ -105,7 +108,7 @@ ISR (SPI_STC_vect)
       digitalWrite(M21, LOW);
       receivedone = true;
     }
-    if(c=='E'){
+    if(c=='Q'){
       analogWrite(speed1, 160); // Set rotating motor speed
       digitalWrite(M11, LOW);
       digitalWrite(M12, HIGH);
@@ -121,7 +124,7 @@ ISR (SPI_STC_vect)
       digitalWrite(M21, LOW);
       receivedone = true;
     }
-    if(c=='T'){
+    if(c=='W'){
       analogWrite(speed1, 160); // Set rotating motor speed
       digitalWrite(M11, HIGH);
       digitalWrite(M12, LOW);
@@ -137,7 +140,10 @@ ISR (SPI_STC_vect)
       digitalWrite(M21, LOW);
       receivedone = true;
     }
+    
+   
     delay(100);
   }
+  
   SREG = oldsrg;
 }
